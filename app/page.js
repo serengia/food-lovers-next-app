51686e9b120364910baab6dd8fcc01f78de9cdc7
James Serengia <jamesserengia@gmail.com>
2024-01-02 20:36:17 +0300

    Update initaials files

--- app/page.js
+++ app/page.js
@@ -1,9 +1,9 @@
 export default function Home() {
   return (
     <main>
-      <img src="/logo.png" alt="A server surrounded by magic sparkles." />
-      <h1>Food lovers app</h1>
-      <p>🔥 Let&apos;s get started! 🔥</p>
+      <h1 style={{ color: 'white', textAlign: 'center' }}>
+        Time to get started!
+      </h1>
     </main>
   );
 }
