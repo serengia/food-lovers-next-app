c1c298fc8eb40cc675185f871ab13475dea3719a
James Serengia <jamesserengia@gmail.com>
2024-01-02 20:02:17 +0300

    Project setup

---
+++ app/page.js
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <main>
+      <img src="/logo.png" alt="A server surrounded by magic sparkles." />
+      <h1>Food lovers app</h1>
+      <p>🔥 Let&apos;s get started! 🔥</p>
+    </main>
+  );
+}
