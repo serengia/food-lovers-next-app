23b926c240e010ad82ae1ac0c289b27c48f89040
James Serengia <jamesserengia@gmail.com>
2024-01-03 08:06:12 +0300

    Implement general page layouts

--- app/meals/page.js
+++ app/meals/page.js
@@ -1,9 +1,42 @@
-import React from "react";
+import { Suspense } from 'react';
+import Link from 'next/link';
 
-export default function page() {
+import classes from './page.module.css';
+import MealsGrid from '@/components/meals/meals-grid';
+import { getMeals } from '@/lib/meals';
+
+export const metadata = {
+  title: 'All Meals',
+  description: 'Browse the delicious meals shared by our vibrant community.',
+};
+
+async function Meals() {
+  console.log('Fetching meals');
+  const meals = await getMeals();
+
+  return <MealsGrid meals={meals} />;
+}
+
+export default function MealsPage() {
   return (
-    <main>
-      <h1>Meals page</h1>
-    </main>
+    <>
+      <header className={classes.header}>
+        <h1>
+          Delicious meals, created{' '}
+          <span className={classes.highlight}>by you</span>
+        </h1>
+        <p>
+          Choose your favorite recipe and cook it yourself. It is easy and fun!
+        </p>
+        <p className={classes.cta}>
+          <Link href="/meals/share">Share Your Favorite Recipe</Link>
+        </p>
+      </header>
+      <main className={classes.main}>
+        <Suspense fallback={<p className={classes.loading}>Fetching meals...</p>}>
+          <Meals />
+        </Suspense>
+      </main>
+    </>
   );
 }
